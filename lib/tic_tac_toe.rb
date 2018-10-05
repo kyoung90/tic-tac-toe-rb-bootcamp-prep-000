07cf3fb9792ebb7e3c8c54bb0232745f6ac742cf
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-05 23:25:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,7 +115,7 @@ def play(board)
   end
   
   if won?(board)
-    puts "Congratulations #{board[won?(board)[0]}!"
+    puts "Congratulations #{board[won?(board)[0]]}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
